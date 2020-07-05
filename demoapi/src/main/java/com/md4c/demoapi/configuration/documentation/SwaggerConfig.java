d31c00da369c555b88f023163ac6df582d9633b5
Felipe Fonseca Rocha <felipefonsecarocha@gmail.com>
2020-07-05 19:58:30 -0300

    SWAGGER - Configurated

--- demoapi/src/main/java/com/md4c/demoapi/configuration/documentation/SwaggerConfig.java
+++ demoapi/src/main/java/com/md4c/demoapi/configuration/documentation/SwaggerConfig.java
@@ -5,9 +5,12 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMethod;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.AuthorizationScope;
@@ -15,8 +18,10 @@ import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.*;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import static com.google.common.collect.Lists.newArrayList;
 import static io.swagger.models.auth.In.HEADER;
 import static java.util.Collections.singletonList;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
@@ -58,6 +63,17 @@ public class SwaggerConfig {
 		                .build())
 		        )
 				.useDefaultResponseMessages(false)
+				.globalResponseMessage(RequestMethod.GET,
+						newArrayList(new ResponseMessageBuilder()
+										.code(500)
+										.message("500 message")
+										.responseModel(new ModelRef("Error"))
+										.build(),
+								new ResponseMessageBuilder()
+										.code(403)
+										.message("Forbidden!")
+										.build()))
+				.useDefaultResponseMessages(false)
 				.apiInfo(apiInfo()).select()
 				.apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.boot")))
 				.paths(PathSelectors.any())
@@ -73,6 +89,26 @@ public class SwaggerConfig {
 				.title(applicationName)
 				.description(applicationDescription)
 				.version(applicationVersion)
+				.build();
+	}
+
+	@Bean
+	UiConfiguration uiConfig() {
+		return UiConfigurationBuilder.builder()
+				.deepLinking(true)
+				.displayOperationId(false)
+				.defaultModelsExpandDepth(1)
+				.defaultModelExpandDepth(1)
+				.defaultModelRendering(ModelRendering.EXAMPLE)
+				.displayRequestDuration(false)
+				.docExpansion(DocExpansion.NONE)
+				.filter(false)
+				.maxDisplayedTags(null)
+				.operationsSorter(OperationsSorter.ALPHA)
+				.showExtensions(false)
+				.tagsSorter(TagsSorter.ALPHA)
+				.supportedSubmitMethods(UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS)
+				.validatorUrl(null)
 				.build();
 	}
 }
